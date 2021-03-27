3af5d0dafda55cefb3abd8ef6cd1b5372f1c7dc6
Angelo Breuer <angelobr@outlook.de>
2021-03-27 12:21:58 +0100

    Fix code visualizer line number padding

--- src/components/Debugger.tsx
+++ src/components/Debugger.tsx
@@ -1,9 +1,19 @@
+const padLine = (index: number) => {
+    index++
+
+    if (index < 10) {
+        return ' ' + index
+    }
+
+    return index
+}
+
 export default function Debugger(props: { line: number, code: string }) {
     return <pre style={{ color: '#fff' }}>
         <code>
             {props.code.split('\n').map((x, i) => <div
                 key={`line-${i}`}
-                className={props.line === i + 1 ? 'line-highlight' : 'no-line-highlight'}>{i + 1} {x.length ? x : <br />}</div>)}
+                className={props.line === i + 1 ? 'line-highlight' : 'no-line-highlight'}>{padLine(i)} {x.length ? x : <br />}</div>)}
         </code>
     </pre>
 }
